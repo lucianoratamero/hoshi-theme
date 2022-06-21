df8b4f050d16c20e1ce4fdf489bb517e4e51a3a3
GitHub Action <action@github.com>
2022-06-21 04:27:00 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/lucianoratamero/hoshitheme/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.lucianoratamero.hoshitheme.services
+
+import com.github.lucianoratamero.hoshitheme.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
